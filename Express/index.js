9417fc3d0733cccdec5e9bcaa6eb6718c3efdc37
akshatverma1 <akshaverma14@gmail.com>
2024-06-30 12:48:08 +0530

    30/06/2024

--- Express/index.js
+++ Express/index.js
@@ -7,3 +7,8 @@ let port =3;
 app.listen(port,()=>{
     console.log(`this is first web server ${port}`);
 })
+
+app.use((req,res)=>{
+    console.log("request received");
+    res.send("Lakshay is a pagal man");
+})
