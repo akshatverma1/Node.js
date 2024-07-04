8c06de8a05d9a5cb7987ccb05b639fd6f82811c2
akshatverma1 <akshaverma14@gmail.com>
2024-07-05 01:43:18 +0530

    Rest Full Api's

--- RestFullApis/index.js
+++ RestFullApis/index.js
@@ -2,14 +2,33 @@ const express = require("express");
 const app = express();
 
 const port = 8080;
+const path = require("path");
 
 
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
+app.use(express.static(path.join(__dirname,"Public")));
 
+let post =[
+    {
+        username : "activa",
+        age : "it's is a very relax two wheelers"
+    },
+    {
+        username : "bike",
+        age : "it's is a two wheelers"
+    },
+    {
+        username : "cars",
+        age :"this is very good car in the market"
+    },
+];
 app.listen(port, () => {
     console.log("Listen Command");
 })
 
 app.get("/",(req,res)=>{
-    res.send("Get Command Invoked");
+    res.render("index.ejs",{post});
 })
