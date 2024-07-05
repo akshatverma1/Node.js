4c4565b133495533b26d437fc4bdfa77f5d8de50
akshatverma1 <akshaverma14@gmail.com>
2024-07-05 14:22:09 +0530

    Rest Full Api's

--- RestFullApis/index.js
+++ RestFullApis/index.js
@@ -13,14 +13,17 @@ app.use(express.static(path.join(__dirname,"Public")));
 
 let post =[
     {
+        id : "1a",
         username : "activa",
         age : "it's is a very relax two wheelers"
     },
     {
+        id : "2a",
         username : "bike",
         age : "it's is a two wheelers"
     },
     {
+        id : "3a",
         username : "cars",
         age :"this is very good car in the market"
     },
@@ -40,4 +43,11 @@ app.post("/post",(req,res)=>{
     post.push({username,age});
     // res.send("accepted");
     res.redirect("http://localhost:8080/post")
+})
+app.get("/post/:id",(req,res)=>{
+    let {id} = req.params;
+    let searchPost = post.find((p)=> id === p.id);
+    console.log(searchPost);
+    // res.send("This is a Single posts");
+    res.render("single.ejs",{searchPost});
 })
